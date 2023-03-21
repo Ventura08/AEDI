6844b30d421fcc6b932dea2cc761711a3f640a02
Ventura08 <estevaotevu08@gmail.com>
2023-03-21 20:50:49 -0300

    school

--- src/Conteudo_7/C07Ex01.java
+++ src/Conteudo_7/C07Ex01.java
@@ -1,7 +1,43 @@
 package Conteudo_7;
 
+import javax.swing.*;
+
+import static ShortcutFunctions.ShortcutFunctions.toInt;
+
 public class C07Ex01 {
     public static void main(String[] args) {
-
+        JTextField name = new JTextField();
+        JTextField points = new JTextField();
+        String result = "";
+        Object[] fields = {
+                "Informe seu nome: ", name,
+                "Informe o numero de acertos: ", points,
+        };
+        JOptionPane.showConfirmDialog(
+                null,
+                fields,
+                "",
+                JOptionPane.OK_CANCEL_OPTION);
+        switch (toInt(points.getText())) {
+            case 1,2,3,4,5:
+                result = "NENHUM";
+                break;
+            case 6,7,8,9,10:
+                result = "OUTRO CARTÂO";
+                break;
+            case 11:
+                result = "R$100,00";
+                break;
+            case 12:
+                result = "R$1000.00";
+                break;
+            case 13:
+                result = "R$50.000";
+        }
+        JOptionPane.showMessageDialog(
+                null,
+                result,
+                "Resultado",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 }
---
+++ src/Conteudo_7/C07Ex02.java
@@ -0,0 +1,5 @@
+package Conteudo_7;
+
+public class C07Ex02 {
+
+}
