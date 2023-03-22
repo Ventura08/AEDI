4f9ec94cc1f42d8336fa674e33ee72c898b919cb
Ventura08 <estevaotevu08@gmail.com>
2023-03-21 21:39:21 -0300

    school

--- src/Conteudo_7/C07Ex04.java
+++ src/Conteudo_7/C07Ex04.java
@@ -1,7 +1,45 @@
 package Conteudo_7;
 
+import javax.swing.*;
+
+import static ShortcutFunctions.ShortcutFunctions.toInt;
+
 public class C07Ex04 {
     public static void main(String[] args) {
-
+        JTextField team = new JTextField();
+        String result = "";
+        Object[] fields = {
+                "Time: ", team,
+        };
+        JOptionPane.showConfirmDialog(
+                null,
+                fields,
+                "",
+                JOptionPane.OK_CANCEL_OPTION);
+        switch (team.getText()){
+            case "América", "Atlético", "Cruzeiro", "Villa Nova":
+                result = "Minas Gerais";
+                break;
+            case "Botafogo", "Flamengo", "Fluminense", "Vasco":
+                result = "Minas Gerais";
+                break;
+            case "Corinthians", "Palmeiras", "Santos", "São Paulo":
+                result = "Minas Gerais";
+                break;
+            case "Grêmio", "Internacional", "Juventude":
+                result = "Minas Gerais";
+                break;
+            case "Náutico", "Santa Cruz", "Sport":
+                result = "Minas Gerais";
+                break;
+            default:
+                result = "Time inválido!";
+                break;
+        }
+        JOptionPane.showMessageDialog(
+                null,
+                result,
+                "Resultado",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 }
