98f9c5e5b660b67819787c2ecf3695a67008a6ab
Ventura08 <estevaotevu08@gmail.com>
2023-04-16 22:33:15 -0300

    .

---
+++ src/Conteudo_9/C09Ex05.java
@@ -0,0 +1,21 @@
+package Conteudo_9;
+
+import javax.swing.*;
+
+public class C09Ex05 {
+    public static void main(String[] args) {
+        StringBuilder result = new StringBuilder();
+        double firstValue = 1;
+        int secondValue = 0;
+        double serie = 0;
+        for (int i = 1; i <= 100; i++, firstValue = Math.pow(i, 2), secondValue = i - 1) {
+            serie += (double) i / (firstValue + secondValue);
+        }
+        result.append(serie);
+        JOptionPane.showMessageDialog(
+                null,
+                result.toString(),
+                "Resultado",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+}
