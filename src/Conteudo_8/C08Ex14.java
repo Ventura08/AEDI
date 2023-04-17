ea0a6b63b89217a35b6e4ede4759e9ef2f6d8eb2
Ventura08 <estevaotevu08@gmail.com>
2023-04-17 20:03:43 -0300

    .

--- src/Conteudo_8/C08Ex14.java
+++ src/Conteudo_8/C08Ex14.java
@@ -1,7 +1,36 @@
 package Conteudo_8;
 
+import javax.swing.*;
+
+import java.awt.*;
+
+import static ShortcutFunctions.ShortcutFunctions.toInt;
+
 public class C08Ex14 {
     public static void main(String[] args) {
-
+        JTextField iterationLenght = new JTextField();
+        JTextField symbol = new JTextField();
+        Object[] fields = {
+                "Digite o numero : ", iterationLenght,
+                "Digite o simbolo que deseja: ", symbol,
+        };
+        JOptionPane.showConfirmDialog(
+                null,
+                fields,
+                "",
+                JOptionPane.OK_CANCEL_OPTION);
+        StringBuilder result = new StringBuilder();
+        for (int i = -1; i < toInt(iterationLenght.getText()); i++) {
+            result.append(symbol.getText().repeat(toInt(iterationLenght.getText())))
+                    .append("<br/>");
+        }
+        final JLabel label = new JLabel();
+        label.setText("<html><body>" + result + "</body></html>");
+        label.setFont(new Font("Arial", Font.BOLD, 19));
+        JOptionPane.showMessageDialog(
+                null,
+                label,
+                "Resultado",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 }
--- src/Conteudo_9/C09Ex05.java
+++ src/Conteudo_9/C09Ex05.java
@@ -1,20 +1,24 @@
 package Conteudo_9;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class C09Ex05 {
     public static void main(String[] args) {
         StringBuilder result = new StringBuilder();
-        double firstValue = 1;
-        int secondValue = 0;
+        double firstValue = 3;
+        int secondValue = 7;
         double serie = 0;
-        for (int i = 1; i <= 100; i++, firstValue = Math.pow(i, 2), secondValue = i - 1) {
-            serie += (double) i / (firstValue + secondValue);
+        for (int i = 0; i < 10; i++, firstValue += 3, secondValue += 6) {
+            serie += (double) 5 * ((i * 2) + firstValue) / secondValue;
         }
         result.append(serie);
+        final JLabel label = new JLabel();
+        label.setText("<html><body>" + result + "</body></html>");
+        label.setFont(new Font("Arial", Font.BOLD, 19));
         JOptionPane.showMessageDialog(
                 null,
-                result.toString(),
+                label,
                 "Resultado",
                 JOptionPane.INFORMATION_MESSAGE);
     }
