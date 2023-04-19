aa27e7ef2592dbb5787db239644a4ba788787d74
Ventura08 <estevaotevu08@gmail.com>
2023-04-18 21:33:00 -0300

    .

--- src/Conteudo_9/C09Ex05.java
+++ src/Conteudo_9/C09Ex05.java
@@ -3,13 +3,23 @@ package Conteudo_9;
 import javax.swing.*;
 import java.awt.*;
 
+import static ShortcutFunctions.ShortcutFunctions.toDouble;
+
 public class C09Ex05 {
     public static void main(String[] args) {
+        JTextField iterationLenght = new JTextField();
+
+        JOptionPane.showConfirmDialog(
+                null,
+                iterationLenght,
+                "Qual sera o numero de repetições?",
+                JOptionPane.OK_CANCEL_OPTION);
+
         StringBuilder result = new StringBuilder();
         double firstValue = 3;
         int secondValue = 7;
         double serie = 0;
-        for (int i = 0; i < 10; i++, firstValue += 3, secondValue += 6) {
+        for (int i = 0; i < toDouble(iterationLenght.getText()); i++, firstValue += 3, secondValue += 6) {
             serie += (double) 5 * ((i * 2) + firstValue) / secondValue;
         }
         result.append(serie);
